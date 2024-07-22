2608d30f1bbf4cadb0793a1ef89f7a65dfe96b74
it-araiguma <notnow618@gmail.com>
2024-07-22 22:56:40 +0900

    Ziggy導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,10 @@
 import './bootstrap';
-import '../css/app.css';
+// import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import { Quasar } from 'quasar';
+import { Quasar, Notify } from 'quasar';
 import quasarLang from 'quasar/lang/ja';
 import '@quasar/extras/roboto-font/roboto-font.css';
 import '@quasar/extras/material-icons/material-icons.css';
@@ -15,15 +14,21 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue')
+        ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
             .use(Quasar, {
-                plugins: {},
+                plugins: {
+                    Notify,
+                },
                 lang: quasarLang
             })
+            .mixin({ methods: { route } })
             .mount(el);
     },
     progress: {
